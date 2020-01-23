ff0996749f4f6479f6234c2ff4eb38e49d983000
Fritz Wallace <john.fritz.wallace@gmail.com>
2020-01-23 16:06:06 -0500

    Added base scripts

---
+++ Assets/PunchAttack.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PunchAttack : BaseAttack
+{
+     
+    
+    
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public bool fireAttack(BlobScript attacker, BlobScript target)
+    {
+        //Not done, needs a lot of work.
+        
+
+        return true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
