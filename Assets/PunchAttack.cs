4261fc465abf9b42cb6bc6d3304f36fe21136ef1
Fritz Wallace <john.fritz.wallace@gmail.com>
2020-01-23 18:10:57 -0500

    Added base punching attack

--- Assets/PunchAttack.cs
+++ Assets/PunchAttack.cs
@@ -19,7 +19,16 @@ public class PunchAttack : BaseAttack
     public bool fireAttack(BlobScript attacker, BlobScript target)
     {
         //Not done, needs a lot of work.
-        
+        if(true)
+        //replace above statement with a range checker
+        {
+            int dmg = attacker.GetAttack() - target.GetDefense();
+            if(dmg < 0)
+            {
+                dmg = 0;
+            }
+            target.TakeDamage(dmg);
+        }
 
         return true;
     }
